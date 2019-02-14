50b413b797b1f4c087ac9e5a630746de90b29060
iChristiano <christiano.dutti@gmail.com>
2019-02-14 16:37:14 +0100

    set up initial project

---
+++ src/Hello.js
@@ -0,0 +1,24 @@
+﻿import React, {Component} from 'react';
+import './Hello.css';
+
+// class Hello extends Component {
+//     render(){
+//         return(
+//             <div className ='f1 tc'>
+//                 <h1 className='HelloContent'>Hello B&R</h1>
+//                 <p>{this.props.greeting}</p>
+//             </div>
+//         );
+//     }
+// }
+
+const Hello = (props) => {
+    return(
+        <div className ='f1 tc'>
+            <h1 className='HelloContent'>Hello B&R</h1>
+            <p>{props.greeting}</p>
+        </div>
+    );
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello my friend'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
