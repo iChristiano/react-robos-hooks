ca42332611e260266e07e0a00002026aca7d1512
iChristiano <iChristiano.mail@gmail.com>
2022-01-22 10:02:55 +0100

    Updates

---
+++ precache-manifest.367bf65b411324898456198879255a56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de8d8f252085cbd1de55",
+    "url": "/react-robos/static/css/main.a580ff40.chunk.css"
+  },
+  {
+    "revision": "de8d8f252085cbd1de55",
+    "url": "/react-robos/static/js/main.de8d8f25.chunk.js"
+  },
+  {
+    "revision": "1cf54b439c5bba64df2e",
+    "url": "/react-robos/static/js/runtime~main.1cf54b43.js"
+  },
+  {
+    "revision": "a3721b76034b8d31f685",
+    "url": "/react-robos/static/css/2.8b52ac23.chunk.css"
+  },
+  {
+    "revision": "a3721b76034b8d31f685",
+    "url": "/react-robos/static/js/2.a3721b76.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robos/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7ffd5d2fa03c043e6b46a24d758c7eb1",
+    "url": "/react-robos/index.html"
+  }
+];
